bdffc5f7990840ce944ccd8d7ab3239c05b2fe4c
ynfeng <fynwin@gmail.com>
2022-03-05 23:07:20 +0800

    feat: api module.

---
+++ api/src/main.rs
@@ -0,0 +1,9 @@
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
---
+++ api/src/resources/application.rs
@@ -0,0 +1,7 @@
+use actix_web::{App, get, Responder, web};
+
+#[get("/{id}/{name}/index.html")]
+async fn index(params: web::Path<(u32, String)>) -> impl Responder {
+    let (id, name) = params.into_inner();
+    format!("Hello {}! id:{}", name, id)
+}
