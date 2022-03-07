4d8ba1f5c8e1a04c238a5b99f59c866f8491da23
ynfeng <fynwin@gmail.com>
2022-03-07 11:39:15 +0800

    feat: add some tests.

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,12 +2,13 @@ use actix_web::{App, HttpServer};
 
 use resources::application;
 
+mod resources;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(application::index))
+    HttpServer::new(|| App::new()
+        .service(application::create_application))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
-
-mod resources;
--- api/src/resources/application.rs
+++ api/src/resources/application.rs
@@ -1,7 +1,37 @@
-use actix_web::{get, Responder, web};
+use actix_web::{HttpResponse, post, web};
+use serde::{Deserialize, Serialize};
 
-#[get("/{id}/{name}/index.html")]
-pub async fn index(params: web::Path<(u32, String)>) -> impl Responder {
-    let (id, name) = params.into_inner();
-    format!("Hello {}! id:{}", name, id)
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ApplicationCreateRequest {
+    name: String,
+}
+
+#[post("/applications")]
+pub async fn create_application(request: web::Json<ApplicationCreateRequest>) -> HttpResponse {
+    HttpResponse::Created().body(request.name.clone())
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{App, http::{self}, test};
+    use actix_web::dev::Service;
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn create_application_should_ok() {
+        let app =
+            test::init_service(App::new().service(create_application))
+                .await;
+
+        let req = test::TestRequest::post()
+            .uri("/applications")
+            .set_json(ApplicationCreateRequest {
+                name: "test-application".to_owned(),
+            })
+            .to_request();
+
+        let resp = app.call(req).await.unwrap();
+        assert_eq!(resp.status(), http::StatusCode::CREATED);
+    }
 }
