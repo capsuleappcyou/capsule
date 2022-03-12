620f2e3d849928083a57b4e2a5819cbe8abb4617
ynfeng <fynwin@gmail.com>
2022-03-12 19:25:14 +0800

    feat: user.

--- core/src/user/mod.rs
+++ core/src/user/mod.rs
@@ -1,4 +1,3 @@
-use std::borrow::Borrow;
 use std::marker::PhantomData;
 
 use crate::user::credential::Credential;
@@ -10,48 +9,56 @@ pub mod repository;
 
 struct User<T> {
     pub user_name: String,
-    // credentials: Box<dyn Credentials<T>>,
+    credentials: Box<dyn Credentials<T>>,
     phantom: PhantomData<T>,
 }
 
-//
+impl<T> User<T> {
+    pub fn new(user_name: String, credentials: Box<dyn Credentials<T>>) -> Self {
+        User {
+            user_name,
+            credentials,
+            phantom: Default::default(),
+        }
+    }
+}
+
 // impl<T> User<T>
 //     where T: Credential {
 //     pub fn add_credential(&self, credential: T) {
-//         self.credentials.add(credential)
+//         // self.credentials.add(credential)
 //     }
 // }
-//
-// #[cfg(test)]
-// mod tests {
-//     use crate::user::credential::Credential;
-//     use crate::user::credential::pwd_credential::PwdCredential;
-//     use crate::user::credentials::Credentials;
-//     use crate::user::User;
-//
-//     struct FakeCredentials {
-//         credentials: Vec<Box<dyn Credential>>,
-//     }
-//
-//     impl FakeCredentials {
-//         pub fn new() -> Self {
-//             FakeCredentials { credentials: vec![] }
-//         }
-//     }
-//
-//     impl<T: Credential> Credentials<T> for FakeCredentials {
-//         fn add(mut self, credential: T) {
-//             self.credentials.push(Box::new(credential))
-//         }
-//     }
-//
-//     #[test]
-//     fn should_add_credential_to_user() {
-//         User {
-//             user_name: String::from("test"),
-//             // credentials: Box::new(FakeCredentials::new()),
-//             // phantom: Default::default(),
-//             phantom: Default::default()
-//         };
-//     }
-// }
+
+#[cfg(test)]
+mod tests {
+    use crate::user::credential::Credential;
+    use crate::user::credential::pwd_credential::PwdCredential;
+    use crate::user::credentials::Credentials;
+    use crate::user::User;
+
+    struct FakeCredentials {
+        credentials: Vec<Box<dyn Credential>>,
+    }
+
+    impl FakeCredentials {
+        pub fn new() -> Self {
+            FakeCredentials { credentials: vec![] }
+        }
+    }
+
+    impl<T: Credential> Credentials<T> for FakeCredentials {
+        fn add(mut self, credential: T) {
+            self.credentials.push(Box::new(credential))
+        }
+    }
+
+    #[test]
+    fn should_create_user() {
+        let user = User::<PwdCredential>::new(
+            String::from("test"),
+            Box::new(FakeCredentials::new()));
+
+        assert_eq!(user.user_name, String::from("test"))
+    }
+}
