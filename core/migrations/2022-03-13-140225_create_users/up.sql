bb4c75f6113089d7ca7c69af84aabbf26e6053bf
ynfeng <fynwin@gmail.com>
2022-03-13 22:06:08 +0800

    feat: some db scripts

---
+++ core/migrations/2022-03-13-140225_create_users/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE capsule_user(
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR NOT NULL,
+                       create_time timestamp
+)
---
+++ core/src/schema.rs
@@ -0,0 +1,7 @@
+table! {
+    capsule_user (id) {
+        id -> Int4,
+        name -> Varchar,
+        create_time -> Nullable<Timestamp>,
+    }
+}
