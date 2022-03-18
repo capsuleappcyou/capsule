6322030c9107c6619a44008c22600812738f88ff
ynfeng <fynwin@gmail.com>
2022-03-18 21:13:00 +0800

    refactor: add pwd credential scripts.

--- capsule-core/migrations/2022-03-13-141511_create_users/up.sql
+++ capsule-core/migrations/2022-03-13-141511_create_users/up.sql
@@ -3,4 +3,5 @@ CREATE TABLE capsule_users
     id        SERIAL PRIMARY KEY,
     name      VARCHAR   NOT NULL,
     create_at timestamp NOT NULL
-)
+);
+create unique index capsule_users_name_uindex on capsule_users (name);
