bae876d6b4c7f4d22851b7a06ef43ca379ad4652
ynfeng <fynwin@gmail.com>
2022-04-09 16:07:59 +0800

    feat: application api impl.

--- capsule-core/migrations/postgres/2022-03-31-152629_create_applications/up.sql
+++ capsule-core/migrations/postgres/2022-03-31-152629_create_applications/up.sql
@@ -6,5 +6,5 @@ CREATE TABLE capsule_applications
     owner                 varchar(200) not null,
     create_at             timestamp    not null
 );
-create index capsule_applications_application_name_index on capsule_applications (application_name);
 
+create unique index capsule_applications_application_name_uindex on capsule_applications (application_name);
